b64f0a282665ae75c34f2d03e61845b90eb3b171
higorfranca96 <117456511+higorfranca96@users.noreply.github.com>
2025-06-09 16:50:07 -0300

    alteração url para produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3000' });
+// const api = axios.create({ baseURL: 'http://localhost:3000' }); //desenvolvimento
+const api = axios.create({ baseURL: 'https://api-todo-auth.onrender.com' }); //produção
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
