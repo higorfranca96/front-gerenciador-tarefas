d4a94005a7458edbd933a86018bfad1b21b04fde
higorfranca96 <117456511+higorfranca96@users.noreply.github.com>
2025-06-16 10:23:55 -0300

    melhorias layout e cores

--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { data, Link } from 'react-router-dom';
 import api from '../services/api';
+import { MdOutlineEdit } from "react-icons/md";
+import { MdOutlineClear } from 'react-icons/md';
 
 export default function TodoListPage() {
   const [tarefas, setTarefas] = useState([]);
@@ -26,33 +28,55 @@ export default function TodoListPage() {
   }, []);
 
   return (
-    <div className='form'>
-      <div className='rowTarefa'>
-
-        {tarefas.map((t) => (
-          <div className='colTarefa' key={t._id}>
-            <div className='infoTarefa'>
-
-              <div className='boxTitleTarefa'>
-                <h3 className='titleTarefa'>{t.titulo}</h3>
-
-              </div>
-              <p>Fazer até <strong>{ajustaData(t.prazo)}</strong></p>
-              <p><strong>{t.concluida ? (<div class='tarefaConcluida'>Concluída</div>) : (<div class='tarefaEmAberto'>Em aberto</div>)}</strong></p>
-
-              <p>{t.descricao}</p>
-
-              <Link className='button' to={`/tarefas/editar/${t._id}`}>Editar</Link>
-              <button className='button' onClick={() => deleteTarefa(t._id)}>Excluir</button>
-            </div>
+    <>
+      <div className='form'>
+        <div className='filtrosBox'>
+          <div className='filtroItem'>
+            <label><p>Título:</p></label>
+            <input type='text'></input>
           </div>
-        ))}
-        {tarefas.length == 0 ? (
-          <div className='semTarefas'>
-            <p>Não há tarefas</p>
+          
+          <div className='filtroItem'>
+            <label><p>Prazo até:</p></label>
+            <input type='text'></input>
           </div>
-          ): ""}
+
+          <div className='filtroItem'>
+            <label><p>Status:</p></label>
+            <input type='text'></input>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='form'>
+        <div className='rowTarefa'>
+
+          {tarefas.map((t) => (
+            <div className='colTarefa' key={t._id}>
+              <div className='infoTarefa'>
+
+                <div className='boxTitleTarefa'>
+                  <h3 className='titleTarefa'>{t.titulo}</h3>
+
+                </div>
+                <p>Fazer até <strong>{ajustaData(t.prazo)}</strong></p>
+                <p><strong>{t.concluida ? (<div class='tarefaConcluida'>Concluída</div>) : (<div class='tarefaEmAberto'>Em aberto</div>)}</strong></p>
+
+                <p>{t.descricao}</p>
+
+                <Link className='button' to={`/tarefas/editar/${t._id}`}><MdOutlineEdit /></Link>
+                <button className='button' onClick={() => deleteTarefa(t._id)}><MdOutlineClear /></button>
+              </div>
+            </div>
+          ))}
+          {tarefas.length == 0 ? (
+            <div className='semTarefas'>
+              <p>Não há tarefas</p>
+            </div>
+            ): ""}
+        </div>
+      </div>
+
+    </>
   );
 }
